00aa83ce49f9cc2681036c7b376b268f79779c70
BuildTools <cat71602@gmail.com>
2018-01-01 15:39:51 -0500

    fixed commands and linted correctly

--- bot.js
+++ bot.js
@@ -1,142 +1,163 @@
 const Eris = require('eris')
-const { CommandOptions, Message } = require('eris')
+const { CommandOptions, Message } = require('eris') // eslint-disable-line no-unused-vars
 const fs = require('fs')
 const readline = require('readline-sync')
 const load = require('./load.js')
 
 class Config {
-    /**
-     * @param {String} token 
-     * @param {String} ownerID
-     */
-    constructor(token, ownerID, defaultPrefix) {
-        this.token = token
-        this.ownerID = ownerID
-        this.defaultPrefix = defaultPrefix
-    }
+  /**
+   * @param {String} token
+   * @param {String} ownerID
+   */
+  constructor (token, ownerID, defaultPrefix) {
+    this.token = token
+    this.ownerID = ownerID
+    this.defaultPrefix = defaultPrefix
+  }
 }
 
 class Catbot {
+  /**
+   * @param {String} directory
+   */
+  constructor (directory) {
+    this.directory = directory
+  }
 
-    /**
-     * @param {String} directory
-     */
-    constructor(directory) {
-        this.directory = directory
+  load () {
+    this.log('Loading...')
+    this.config = this.getConfig()
+    this.tools = load(`${this.directory}/tools`)
+    this.client = new Eris.CommandClient(this.config.token, {}, { owner: this.config.ownerID, prefix: this.config.defaultPrefix })
+    let events = load(`${this.directory}/events`)
+    for (let event in events) {
+      if (events[event] instanceof Function) {
+        this.client.on(event, (data) => {
+          events[event](data, this)
+        })
+      } else {
+        this.log(`Could not load event '${event}': No function was exported`)
+      }
+    }
+    let commands = load(`${this.directory}/commands`)
+    for (let cmd in commands) {
+      /**
+       * @type {Command}
+       */
+      let command = commands[cmd](this)
+      this.log(require('util').inspect(command.options))
+      this.client.registerCommand(command.name, (msg, args) => {
+        command.prepare(this)
+        command.run(msg, args, this)
+      }, command.options)
+    }
+    this.loaded = true
+    this.log('Loaded.')
+  }
+
+  connect () {
+    if (this.loaded) {
+      this.log('Connecting...')
+      this.client.on('ready', () => {
+        this.log('Connected.')
+      })
+      this.client.connect()
+    } else {
+      throw new Error('Bot could not connect: Bot not loaded!')
+    }
+  }
+
+  /**
+   * @return {Config}
+   */
+  getConfig () {
+    let CONFIG_FILE = 'config.json'
+    let neededConfig = new Config()
+
+    let writeConfig = (config) => {
+      fs.writeFileSync(`${this.directory}/${CONFIG_FILE}`, JSON.stringify(config, null, '\t'))
     }
 
-    load() {
-        this.log('loading...')
-        this.config = this.getConfig()
-        this.tools = load(`${this.directory}/tools`)
-        this.client = new Eris.CommandClient(this.config.token, {}, {owner: this.config.ownerID, prefix: this.config.defaultPrefix})
-        let events = load(`${this.directory}/events`)
-        for (let event in events) {
-            if (events[event] instanceof Function) this.client.on(event, (data) => {
-                events[event](data, this)
-            })
-            else this.log(`Could not load event '${event}': No function was exported`)
+    if (!fs.existsSync(`${this.directory}/${CONFIG_FILE}`)) {
+      this.log('No config file detected!\nCreating new config file...')
+      let config = neededConfig
+      for (let key in config) {
+        config[key] = this.getInput(`Enter ${key}`)
+      }
+      writeConfig(config)
+      this.log('Config file generated')
+      return config
+    } else {
+      let config = require(`${this.directory}/${CONFIG_FILE}`)
+      let updated = false
+      for (let key in neededConfig) {
+        if (config[key] == null) {
+          this.log(`No ${key} found in config!`)
+          config[key] = this.getInput(`Enter ${key}`)
+          updated = true
         }
-        /**@type {Command[]} */
-        let commands = load(`${this.directory}/commands`)
-        for (let cmd in commands) {
-            let command = commands[cmd](this)
-            this.client.registerCommand(command.name, (msg, args) => {
-                command.prepare(this)
-                command.run(msg, args, this)
-            }, command.options)
-        }
-        this.loaded = true
-        this.log('loaded.')
+      }
+      if (updated) {
+        writeConfig(config)
+        this.log('Config file updated.')
+      }
+      return config
     }
+  }
 
-    connect() {
-        if (this.loaded) {
-            this.log('connecting...')
-            this.client.on('ready', () => {
-                this.log('connected.')
-            })
-            this.client.connect()
-        } else {
-            throw new Error('Bot could not connect: Bot not loaded!')
-        }
-    }
+  /**
+   * @param {string} msg
+   * @return {string}
+   */
+  getInput (msg) {
+    return readline.question(this.getLogString(`${msg}: `))
+  }
 
-    /**
-     * @return {Config}
-     */
-    getConfig() {
-        let CONFIG_FILE = 'config.json'
-        let neededConfig = new Config()
-        if (!fs.existsSync(`${this.directory}/${CONFIG_FILE}`)) {
-            this.log('No config file detected!\nCreating new config file...')
-            let config = neededConfig
-            for (let key in config) {
-                config[key] = readline.question(this.getLogString(`Enter ${key}:`))
-            }
-            fs.writeFileSync(`${this.directory}/${CONFIG_FILE}`, JSON.stringify(config, null, '\t'))
-            this.log('Config file generated')
-            return config
-        } else {
-            let config = require(`${this.directory}/${CONFIG_FILE}`)
-            for(let key in neededConfig){
-                if(config[key] == null){
-                    this.log(`No ${key} found in config!`)
-                    config[key] = readline.question(this.getLogString(`Enter ${key}:`))
-                }
-            }
-            return config
-        }
-    }
+  /**
+   * generates the string `log(msg)` outputs
+   * @param {string} msg
+   * @return {string}
+   */
+  getLogString (msg) {
+    return `[bot] ${msg}`
+  }
 
-    /**
-     * generates the string `log(msg)` outputs
-     * @param {string} msg 
-     * @return {string}
-     */
-    getLogString(msg){
-        return `[bot] ${msg}`
-    }
-
-    /**
-     * logs a message to the console
-     * @param {string} msg 
-     */
-    log(msg) {
-        console.log(this.getLogString(msg))
-    }
+  /**
+   * logs a message to the console
+   * @param {string} msg
+   */
+  log (msg) {
+    console.log(this.getLogString(msg))
+  }
 }
 
 let Command = class Command {
+  /**
+   * @param {String} name
+   * @param {function(Message, String[], Catbot)} run
+   * @param {CommandOptions} [options]
+   */
+  constructor (name, run, options) {
+    this.name = name
+    this.run = run
+    this.options = options
+  }
 
-    /**
-     * @param {String} name 
-     * @param {function(Message, String[], Catbot)} run
-     * @param {CommandOptions} [options] 
-     */
-    constructor(name, run, options){
-        this.name = name
-        this.run = run
-        this.options = options
-    }
+  /**
+   * @param {Catbot} bot
+   */
+  prepare (bot) {
+    this.bot = bot
+  }
 
-    /**
-     * @param {Catbot} bot 
-     */
-    prepare(bot){
-        this.bot = bot
-    }
-
-    /**
-     * logs a message
-     * @param {string} msg 
-     */
-    log(msg) {
-        this.bot.log(`[Command:${this.name}] ${msg}`)
-    }
+  /**
+   * logs a message
+   * @param {string} msg
+   */
+  log (msg) {
+    this.bot.log(`[Command:${this.name}] ${msg}`)
+  }
 }
-
-let test = new Command('', (msg, args) => {}, {})
 
 Catbot.Command = Command
 module.exports = Catbot
--- load.js
+++ load.js
@@ -2,45 +2,45 @@ const fs = require('fs')
 const path = require('path')
 
 /**
- * @param {string} source 
+ * @param {string} source
  * @return {boolean}
  */
 let isFile = source => fs.lstatSync(source).isFile()
 
 /**
  * gets all files in a directory
- * @param {string} directory 
+ * @param {string} directory
  * @return {string[]}
  */
 let getFiles = (directory) => {
-    let files = fs.readdirSync(directory).map(name => path.join(directory, name)).filter(isFile)
-    for (let file in files) {
-        files[file] = files[file].slice(directory.length + 1)
-    }
-    return files
+  let files = fs.readdirSync(directory).map(name => path.join(directory, name)).filter(isFile)
+  for (let file in files) {
+    files[file] = files[file].slice(directory.length + 1)
+  }
+  return files
 }
 
 let log = msg => {
-    console.log(`[load.js] ${msg}`)
+  console.log(`[load.js] ${msg}`)
 }
 
 let load = (directory) => {
 
-    if(!fs.existsSync(directory)){
-        fs.mkdirSync(directory)
-    }
+  if (!fs.existsSync(directory)) {
+    fs.mkdirSync(directory)
+  }
 
-    let obj = {}
-    for(let file of getFiles(directory)){
-        if(file.endsWith('.js')) {
-            try {
-                obj[file.slice(0, -3)] = require(`${directory}/${file}`)
-            } catch (ex) {
-                log(`Failed to load file '${file}'! : ${ex}`)
-            }
-        }
+  let obj = {}
+  for (let file of getFiles(directory)) {
+    if (file.endsWith('.js')) {
+      try {
+        obj[file.slice(0, -3)] = require(`${directory}/${file}`)
+      } catch (ex) {
+        log(`Failed to load file '${file}'! : ${ex}`)
+      }
     }
-    return obj
+  }
+  return obj
 }
 
-module.exports = load;
+module.exports = load
