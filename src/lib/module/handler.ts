bc0e1dcf2528e62c5a0d9d561aacbb6587f99967
cat16 <cat71602@gmail.com>
2018-06-28 02:09:17 -0400

    finally finished managers

--- src/lib/module/handler.ts
+++ src/lib/module/handler.ts
@@ -1,31 +1,48 @@
+// TODO: if microsoft decides to fix issue 5863 for typescipt change T to this in some places
+
 import { requireDirectory, pathExists, createDirectory, DirectoryContents, loadFile } from '../util/util'
 import Logger from '../util/logger'
 
-export abstract class Element {
-  abstract getTriggers(): string[]
+export interface Element {
+
+  getTriggers(): string[]
+
 }
 
-export abstract class RecursiveElement extends Element {
+let isElement = (obj): obj is Element => {
+  return 'getTriggers' in obj
+}
 
-  private manager?: ElementManager<this>
-  private elements: ElementManager<this>
+export type ElementGenerationFunction<T extends Element> = (rawElement: any) => T
 
-  constructor() {
-    super()
+export interface RecursiveElement extends Element {
+
+  getSubelements(): ElementManager<this>
+
+}
+
+let isRecursiveElement = (obj): obj is RecursiveElement => {
+  return 'getSubelements' in obj && isElement(obj)
+}
+
+export class ElementCategory<T extends RecursiveElement> implements Element {
+
+  private elements: ElementManager<T | ElementCategory<T>>
+  public triggers: string[]
+
+  constructor(directory: string, generateElement: ElementGenerationFunction<T>) {
+    this.elements = new RecursiveElementManager<T>(directory, generateElement, this)
+    this.triggers.push(...directory.split('.'))
   }
 
-  getElements(): ElementManager<this> {
+  getElements(): ElementManager<T | ElementCategory<T>> {
     return this.elements
   }
 
-  getManager(): ElementManager<this> {
-    return this.manager
+  getTriggers(): string[] {
+    return this.triggers
   }
 
-  setManager(manager: ElementManager<this>): void { 
-    if(this.manager === undefined) this.manager = manager
-    else throw new Error('Manager is already set!')
-  }
 }
 
 export class ElementData<T extends Element> {
@@ -44,39 +61,43 @@ export interface ElementMatch<T extends Element> {
   index: number
 }
 
-export type ElementGenerationFunction<T extends Element> = (rawElement: any) => T
-export type GroupGenerationFunction<T extends Element> = (name: string) => T
+export abstract class ElementManager<T extends Element> {
 
-export class ElementManager<T extends Element> {
-
-  private parent?: RecursiveElement
-  private name: string
+  private directory: string
   private elementData: ElementData<T>[]
-  private generateElement: ElementGenerationFunction<T>
+  private recursive: boolean
 
-  constructor(generateElement, parent?: RecursiveElement) {
+  constructor(
+    directory: string,
+    recursive: boolean,
+  ) {
+    this.directory = directory
     this.elementData = []
-    this.parent = parent
-    this.generateElement = generateElement
+    this.recursive = recursive
+  }
+
+  add(data: ElementData<T>): void {
+    this.getElementData().push(data)
   }
 
   remove(index: number): ElementData<T> {
     return this.elementData.splice(index, 1)[0]
   }
 
-  set(index: number, data: ElementData<T>): ElementData<T> {
+  set(index: number, data: ElementData<T>)
+    : ElementData<T> {
     return this.elementData.splice(index, 1, data)[0]
   }
 
   find(trigger: string): ElementMatch<T> {
-    for(let i = 0; i < this.elementData.length; i++) {
+    for (let i = 0; i < this.elementData.length; i++) {
       let element = this.elementData[i].element
       let triggerFound = element.getTriggers().find(t => trigger.startsWith(t))
       if (triggerFound) {
         let r = trigger.slice(triggerFound.length)
-        if(element instanceof RecursiveElement) {
-          let match = element.getElements().find(r)
-          if(match) return match
+        if (isRecursiveElement(element)) {
+          let match = element.getSubelements().find(r)
+          if (match) return match
         }
         return {
           data: this.elementData[i],
@@ -93,45 +114,94 @@ export class ElementManager<T extends Element> {
     this.elementData = []
   }
 
-  private loadFiles(files: Map<string, any>, recursive: boolean, generateGroup?: GroupGenerationFunction<T>): Error[] {
-    let errors: Error[] = []
-    files.forEach((rawElement, name) => {
-      try {
-        let element: T = this.generateElement(rawElement)
-        this.elementData.push(new ElementData<T>(element, `${this.getPath()}/${name}`))
-      } catch (err) {
-        errors.push(err)
-      }
-    })
-    if(recursive)
-    return errors
+  abstract loadContents(contents: DirectoryContents): Map<string, Error>
+
+  load(): Map<string, Error> {
+    return this.loadContents(requireDirectory(this.getPath(), this.recursive))
   }
 
-  load(recursive: boolean, generateGroup?: GroupGenerationFunction<T>): Map<string, Error> {
+  getPath(): string {
+    return this.directory
+  }
+
+  getElementData(): ElementData<T>[] {
+    return this.elementData
+  }
+}
+
+export class FlatElementManager<T extends Element> extends ElementManager<T> {
+
+  private generateElement: ElementGenerationFunction<T>
+
+  constructor(directory: string, recursive: boolean, generateElement: ElementGenerationFunction<T>) {
+    super(directory, recursive)
+    this.generateElement = generateElement
+  }
+
+  loadContents(contents: DirectoryContents): Map<string, Error> {
     let errors: Map<string, Error> = new Map<string, Error>()
-    let loadedDir = requireDirectory(this.getPath(), recursive)
-    loadedDir.files.forEach((rawElement, name) => {
+    contents.files.forEach((rawElement, file) => {
       try {
         let element: T = this.generateElement(rawElement)
-        this.elementData.push(new ElementData<T>(element, `${this.getPath()}/${name}`))
+        this.add(new ElementData<T>(element, `${this.getPath()}/${file}`))
       } catch (err) {
-        errors.set(name, err)
+        errors.set(file, err)
       }
     })
-    errors = new Map<string, Error>([...errors, ...loadedDir.errors])
-    if (recursive) {
-      
-    }
+    errors = new Map<string, Error>([...errors, ...contents.errors])
+    return errors
+  }
+}
+
+export class RecursiveElementManager<T extends RecursiveElement> extends ElementManager<T | ElementCategory<T>> {
+
+  private generateElement: ElementGenerationFunction<T>
+  private parent?: ElementCategory<T>
+
+  constructor(directory: string, generateElement: ElementGenerationFunction<T>, parent?: ElementCategory<T>) {
+    super(directory, true)
+    this.generateElement = generateElement
+    this.parent = parent
+  }
+
+  loadContents(contents: DirectoryContents): Map<string, Error> {
+    let errors: Map<string, Error> = new Map<string, Error>()
+    let rawRecursiveElements: Map<string, any> = new Map<string, any>()
+    contents.files.forEach((rawElement, file) => {
+      if (!(this.generateElement && contents.directories.get(file))) {
+        try {
+          let element: T = this.generateElement(rawElement)
+          this.add(new ElementData<T>(element, `${this.getPath()}/${file}`))
+        } catch (err) {
+          errors.set(file, err)
+        }
+      } else {
+        rawRecursiveElements.set(file, rawElement)
+        contents.directories.delete(file)
+      }
+    })
+    errors = new Map<string, Error>([...errors, ...contents.errors])
+    contents.directories.forEach((contents2, name) => {
+      let path = `${this.getPath()}/${name}`
+      this.add(new ElementData(new ElementCategory(path, this.generateElement), path))
+    })
+    rawRecursiveElements.forEach((rawElement, file) => {
+      let element = this.generateElement(rawElement)
+      this.add(new ElementData<T>(element, `${this.getPath()}/${file}`))
+    })
     return errors
   }
 
-  getParent(): RecursiveElement {
+  getParent(): ElementCategory<T> {
     return this.parent
   }
 
   getPath(): string {
-    return this.parent && this.parent.getManager() ? `${this.parent.getManager().name}/${this.name}` : this.name
+    return this.parent
+      ? `${this.parent.getElements().getPath()}/${super.getPath()}`
+      : super.getPath()
   }
+
 }
 
 export interface LoadOptions<T extends Element> {
@@ -145,7 +215,7 @@ export interface LoadedDirectory<T extends Element> {
   options: LoadOptions<T>
 }
 
-export abstract class ElementHandler<T extends Element> {
+export abstract class ElementHandler<T extends ElementManager<T extends Element>> {
 
   logger: Logger
   manager: ElementManager<T>
@@ -163,29 +233,7 @@ export abstract class ElementHandler<T extends Element> {
     options.manager = options.manager || this.manager
     options.generateFolders = options.generateFolders || false
     if (options.generateFolders && !pathExists(path)) createDirectory(path)
-    let loadedDir = requireDirectory(path, options.recursive)
-    loadedDir.files.forEach((element, file) => {
-      options.manager.add(element, `${path}/${file}`)
-    })
-    if(options.recursive) {
-      loadedDir.directories.forEach((dir, name) => {
-        let match = this.manager.find(name)
-        let manager: ElementManager<T>
-        if (match && match.data.element instanceof RecursiveElement) {
-          manager = match.data.element.getManager()
-        } else {
-          manager = 
-        }
-        this.loadDirectory(`${path}/${name}`, {
-          recursive: true,
-          generateFolders: options.generateFolders,
-          manager: manager
-        })
-      })
-    }
-    loadedDir.errors.forEach((err, file) => {
-      this.logger.error(`Could not load ${this.elementName} from file '${file}': ${err.stack}`)
-    })
+
     this.loadedDirectories.push({
       path: path,
       options: options
@@ -207,7 +255,7 @@ export abstract class ElementHandler<T extends Element> {
 
   reloadElement(trigger: string): boolean {
     let match = this.manager.find(trigger)
-    if(match) {
+    if (match) {
       match.manager.set(match.index, match.manager.)
     }
 
